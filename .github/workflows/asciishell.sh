be442e338d4ab0f647af0c94568b2566245d39e6
ygikk2 <166190409+ygikk2@users.noreply.github.com>
2024-10-07 17:29:06 -0500

    change

---
+++ .github/workflows/asciishell.sh
@@ -0,0 +1,8 @@
+echo "my first "
+ls -la
+cat README.md
+echo "install cowsay"
+sudo apt-get install cowsay -y
+cowsay -z dragon "run for cover, I AM A DRAGON..!!!AAHHAA" >> gragon.txt
+cat gragon.txt
+ls -ltra
